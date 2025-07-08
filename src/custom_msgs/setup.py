130aee4da247267855d0cba038252e7b63507e71
ratulKabir <raihankabirratul@gmail.com>
2025-07-08 20:13:43 +0000

    Add: custom msg pkg to publish end tracking point and grip staate

---
+++ src/custom_msgs/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'custom_msgs'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/msg', ['msg/HandState.msg', 'msg/DualHandState.msg']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='raihankabirratul@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
