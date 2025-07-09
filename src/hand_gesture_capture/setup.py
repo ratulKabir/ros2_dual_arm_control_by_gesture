e34a152399835ecbc3c68032072ec864da2796fa
ratulKabir <raihankabirratul@gmail.com>
2025-07-09 09:05:37 +0000

    Update: custom_msgs added as dependency for hand gesture pkg

--- src/hand_gesture_capture/setup.py
+++ src/hand_gesture_capture/setup.py
@@ -11,7 +11,7 @@ setup(
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'custom_msgs'],
     zip_safe=True,
     maintainer='root',
     maintainer_email='raihankabirratul@gmail.com',
