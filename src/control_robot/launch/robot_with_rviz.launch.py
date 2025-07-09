da4495b53d380cd27205063323c13ebf64373e10
ratulKabir <raihankabirratul@gmail.com>
2025-07-09 14:39:43 +0000

    Update: cleanup

---
+++ src/control_robot/launch/robot_with_rviz.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    pkg_share = FindPackageShare('control_robot').find('control_robot')
+    urdf_file = os.path.join(pkg_share, 'urdf', '2dof.urdf')
+    rviz_config_file = os.path.join(pkg_share, 'config', '2dof.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_file).read()}],
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file],
+        ),
+    ])
